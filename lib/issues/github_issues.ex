fa6c2a185c50245e1da05d5901ba5999de49be3b
Jaya Wijono from Dot Kingdom <jaya@hhd.com.au>
2015-11-13 23:00:19 +1100

    add application configuration, and convert response from HTTPoison

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,30 +1,21 @@
-# defmodule Issues.GithubIssues do
-#   @user_agent [ {"User-agent", "Elixir dave@pragprog.com"} ]
-#
-#   def fetch(user, project) do
-#     issues_url(user, project)
-#     |> HTTPoison.get(@user_agent)
-#     |> handle_response
-#   end
-#
-#   def issues_url(user, project) do
-#     "https://api.github.com/repos/#{user}/#{project}/issues"
-#   end
-#   def handle_response(%{status_code: 200, body: body}), do: { :ok, body }
-#   def handle_response(%{status_code: _, body: body}), do: {:error, body}
-#
-# end
-
 defmodule Issues.GithubIssues do
+  
   @user_agent [ {"User-agent", "u-got-issues"} ]
+
+  # user a module attribute to fetch the value at compile time
+  @github_url Application.get_env(:issues, :github_url)
+
   def fetch(user, project) do 
     issues_url(user, project)
     |> HTTPoison.get(@user_agent) 
     |> handle_response
   end
+
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
-  def handle_response({:ok, %{status_code: 200, body: body}}), do: { :ok, body } 
-  def handle_response({:error, %{status_code: ___, body: body}}), do: { :error, body }
+
+  def handle_response({:ok, %{status_code: 200, body: body}}), do: { :ok, :jsx.decode(body) } 
+  def handle_response({:error, %{status_code: ___, body: body}}), do: { :error, :jsx.decode(body) }
+
 end
