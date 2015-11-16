906fd901973e868e370a52547b3d59e93e4a1e98
Jaya Wijono from Dot Kingdom <jaya@hhd.com.au>
2015-11-16 16:13:33 +1100

    add logging feature

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,4 +1,6 @@
 defmodule Issues.GithubIssues do
+
+  require Logger
   
   @user_agent [ {"User-agent", "u-got-issues"} ]
 
@@ -6,6 +8,7 @@ defmodule Issues.GithubIssues do
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do 
+  Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent) 
     |> handle_response
@@ -15,7 +18,14 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}), do: { :ok, :jsx.decode(body) } 
-  def handle_response({:error, %{status_code: ___, body: body}}), do: { :error, :jsx.decode(body) }
+  def handle_response({:ok, %{status_code: 200, body: body}}) do 
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
+    { :ok, :jsx.decode(body) } 
+  end
+  def handle_response({:error, %{status_code: status, body: body}}) do 
+    Logger.error "Error #{status} returned"
+    { :error, :jsx.decode(body) }
+  end
 
 end
