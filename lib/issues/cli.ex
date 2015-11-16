c8996bd339b56338fb0313404d00c16a5a0180b5
Jaya Wijono from Dot Kingdom <jaya@hhd.com.au>
2015-11-16 15:47:13 +1100

    make a command-line executable

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,11 +1,12 @@
 defmodule Issues.CLI do
+   import Issues.TableFormatter, only: [print_table_for_columns: 2]
 
   @default_count 4
 
   @moduledoc """
   Handle the command line parsing and the dispatch to the various functions that end up generating a table of the last _n_ issues in a github project
   """
-  def run(argv) do
+  def main(argv) do
     argv
       |> parse_args
       |> process
@@ -41,7 +42,7 @@ defmodule Issues.CLI do
     |> convert_to_list_of_hashdicts
     |> sort_into_ascending_order
     |> Enum.take(count)
-    |> Issues.TableFormatter.print_table_for_columns(["number", "created_at", "title"])
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
